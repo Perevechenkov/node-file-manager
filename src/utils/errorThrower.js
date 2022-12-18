9a710031ca32c23980b3dfb5ef7f8d5089248ff4
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-19 01:12:38 +0500

    fix(errors): optional message output

--- src/utils/errorThrower.js
+++ src/utils/errorThrower.js
@@ -1,9 +1,9 @@
 export const throwInvalidInput = err => {
-  console.log(err.message);
+  err ? console.log(err.message) : '';
   throw new Error('Invalid input');
 };
 
 export const throwOperationFailed = err => {
-  console.log(err.message);
+  err ? console.log(err.message) : '';
   throw new Error('Operation failed');
 };
