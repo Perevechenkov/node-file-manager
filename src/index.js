e0bdf2067f2055b9cb3f07c03038b9adc3167fc0
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-18 20:27:47 +0500

    feat(base): use i/o interface to close program

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-const args = Object.fromEntries(process.argv.map(arg => arg.split('=')));
+import readlinePromises from 'node:readline/promises';
 
-console.log(`Welcome to the File Manager, ${args['--username']}!`);
+const args = Object.fromEntries(process.argv.map(arg => arg.split('=')));
+const username = args['--username'];
+console.log(`Welcome to the File Manager, ${username}!`);
+
+const rl = readlinePromises.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', line => {
+  if (/^.exit/.test(line)) rl.close();
+  console.log(`recieved ${line}`);
+}).on('close', () =>
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`)
+);
