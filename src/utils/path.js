fb1fc6c54c6090c596ef9af157f0f96638206ff3
V.V.Perevechenkov <v.v.perevechenkov@urfu.ru>
2022-12-20 00:52:33 +0500

    refactor: add prompt & style

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,6 +1,9 @@
 import path from 'path';
 
-export default () => console.log(`You are currently in ${process.cwd()}`);
+export default () => {
+  console.log(`You are currently in \x1b[33m${process.cwd()} \x1b[0m`);
+  console.log('\x1b[32mPrint some command and see what happens\x1b[0m');
+};
 
 export const getNormalizedPath = receivedPath => {
   const normalizedPath = path.normalize(receivedPath);
